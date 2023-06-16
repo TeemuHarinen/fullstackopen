9e5ab6e0001c809e87e5c145c04ef8e074f6d39b
Teemu Harinen <teemuharinen1@gmail.com>
2023-06-16 15:40:53 +0300

    exercises 1.4 and 1.5

--- osa1/src/App.js
+++ osa1/src/App.js
@@ -48,7 +48,37 @@ const Total = (props) => {
 }
 
   const App = () => {
-    const course = 'Half Stack application development'
+    const course = {
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7
+        },
+        {
+          name: 'State of a component',
+          exercises: 14
+        }
+      ]
+    }
+    
+  return (
+    <div>
+      <Header text={course.name} />
+      <Content parts={course.parts} />
+      <Total exercises={course.parts} />
+    </div>
+  )
+}
+
+export default App
+
+/*
+const course = 'Half Stack application development'
   
     const parts = [
       {
@@ -64,14 +94,4 @@ const Total = (props) => {
         exercises: 14
       }
     ]
-    
-  return (
-    <div>
-      <Header text={course} />
-      <Content parts={parts} />
-      <Total exercises={parts} />
-    </div>
-  )
-}
-
-export default App
+*/
