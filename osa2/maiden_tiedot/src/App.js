59a6cc943753d4d65e6e2f93bdbe19fad8f15b33
Teemu Harinen <teemuharinen1@gmail.com>
2023-08-28 18:08:44 +0300

    added maiden_tiedot

---
+++ osa2/maiden_tiedot/src/App.js
@@ -0,0 +1,76 @@
+import { useState, useEffect } from 'react'
+import dataService from './services/dataHandler'
+
+
+const Filter = ({filterName, handleFilterChange}) => {
+  return (
+    <p>Find countries <input value={filterName} onChange={handleFilterChange}></input></p>
+  )
+}
+
+const Countries = ({countriesToShow, countries}) => {
+  if (countriesToShow.length > 10) {
+    return 'Too many matches, specify another filter'
+
+  } else if (countriesToShow.length <= 10 && countriesToShow.length > 1) {
+    return (<>{countriesToShow.map(country => <li key={country}> {country} </li>)}</>)
+
+  } else if (countriesToShow.length === 1) {
+      const nameToFind = countriesToShow[0]
+      const country = countries.filter(country => country.name.common === nameToFind)
+      const c = country[0]
+      const languages = Object.values(c.languages)
+      const flag = c.flags.png
+      
+      console.log(flag)
+      return (
+        <>
+          <h1> {c.name.common} </h1>
+          <p>capital {c.capital} </p>
+          <p>area {c.area}</p>
+          <h2> languages </h2>
+          <ul>
+            {languages.map(language => <li key={language}> {language} </li>)}
+          </ul>
+          <img src={flag} alt="country-flag"></img>
+
+        </>
+      )
+  }
+}
+
+
+const App = () => {
+
+  const [countries, setCountries] = useState([])
+  const [filterName, setFilterName] = useState('')
+  const [countriesToShow, setCountriesToShow] = useState([])
+
+  const hook = () => {
+    dataService
+      .getAll()
+      .then(data => {
+        setCountries(data)
+
+      })
+  }
+  useEffect(hook, [])
+
+  const handleFilterChange = (event) => {
+    setFilterName(event.target.value)
+    const countryNames = countries.map(country => country.name.common)
+    const tempCountries = countryNames.filter(country => country.toLowerCase().includes(event.target.value.toLowerCase()))
+    setCountriesToShow(tempCountries)
+  }
+
+  return (
+    <div>
+      <h2>Find countries</h2>
+      <Filter filterName={filterName} handleFilterChange={handleFilterChange}></Filter>
+      <Countries countriesToShow={countriesToShow} countries={countries}>  </Countries>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ osa2/maiden_tiedot/src/services/dataHandler.js
@@ -0,0 +1,26 @@
+import axios from 'axios'
+const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api/all'
+
+const getAll = () => {
+    const request = axios.get(baseUrl)
+    return request.then(response => response.data)
+}
+
+const create = newObject => {
+    const request = axios.post(baseUrl, newObject)
+    return request.then(response => response.data)
+}
+
+const update = (id, newNumber) => {
+    const request = axios.put(`${baseUrl}/${id}`, newNumber)
+    return request.then(response => response.data)
+}
+
+const remove = (id) => {
+    const request = axios.delete(`${baseUrl}/${id}`)
+    return request.then(response => response.data)
+}
+
+const exports = { getAll, create, update, remove }
+
+export default exports
