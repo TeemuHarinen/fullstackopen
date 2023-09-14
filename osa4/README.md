8deeff5dfb2cdbf80969f3bacce1cca106897a69
Teemu Harinen <teemuharinen1@gmail.com>
2023-09-14 19:44:51 +0300

    part 4 initialized

--- osa4/README.md
+++
@@ -1,3 +0,0 @@
-# fullstackopen_part3
-
-https://hidden-snow-7258.fly.dev/
--- osa4/index.js
+++ osa4/index.js
@@ -1,113 +1,43 @@
-require('dotenv').config()
+const http = require('http')
 const express = require('express')
-const Person = require('./models/person')
-
-const morgan = require('morgan')
-morgan.token('body', (req) => JSON.stringify(req.body))
-const cors = require('cors')
 const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
 
-app.use(express.static('build'))
-app.use(express.json())
-app.use(morgan(':method :url :status :response-time ms - :body'))
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = 'mongodb+srv://teemuharinen:Oophie4x@cluster0.ptp4sds.mongodb.net/personApp?retryWrites=true&w=majority'
+mongoose.connect(mongoUrl)
+
 app.use(cors())
+app.use(express.json())
 
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-})
-
-app.get('/api/persons', (req, response) => {
-  Person.find({}).then(people => {
-    response.json(people)
-  })
-})
-
-app.get('/info', (req, response, next) => {
-  const currentTime = new Date().toString()
-  Person.countDocuments({})
-    .then(countDocuments => {
-      const text = `Phonebook has info for ${countDocuments} people <br/>` + currentTime
-      response.send(text)
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
     })
-    .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response, next) => {
-  Person.findById(request.params.id)
-    .then(person => {
-      if (person) {
-        response.json(person)
-      } else {
-        response.status(404).end()
-      }
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
     })
-    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response, next) => {
-  Person.findByIdAndRemove(request.params.id)
-    .then(() => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
-})
-
-app.post('/api/persons', (request, response, next) => {
-  const body = request.body
-
-  if (!body.name || !body.number) {
-    return response.status(400).json({
-      error: 'name and number are required'
-    })
-  }
-  const person = new Person({
-    name: body.name,
-    number: body.number
-  })
-
-  person.save()
-    .then(savedPerson => {
-      response.json(savedPerson)
-    })
-    .catch(error => next(error))
-})
-
-app.put('/api/persons/:id', (request, response, next) => {
-  const { name, number } = request.body
-
-  Person.findByIdAndUpdate(
-    request.params.id,
-    { name, number },
-    { new: true, runValidators: true, context: 'query' }
-  )
-    .then(updatedPerson => {
-      response.json(updatedPerson)
-    })
-    .catch(error => next(error))
-})
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
-// Olemattomien osoitteiden käsittely
-app.use(unknownEndpoint)
-
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
-  }
-
-  next(error)
-}
-// Virheellisten pyyntöjen käsittely
-app.use(errorHandler)
-
-const PORT = process.env.PORT || 3000
+const PORT = 3003
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
