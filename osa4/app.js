cbd3ef401ef06db82236c61f21bdaac27e31ba44
Teemu Harinen <teemuharinen1@gmail.com>
2023-09-14 20:27:44 +0300

    new file structure

---
+++ osa4/app.js
@@ -0,0 +1,25 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+// const middleware = require('middleware')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+logger.info('Connecting to', config.MONGODB_URL)
+mongoose.connect(config.MONGODB_URL)
+  .then(() => {
+    logger.info('Connected to MONGODB')
+  })
+  .catch((error) => {
+    logger.error('Error connecting to MONGODB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
