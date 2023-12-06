9c8280d1d38ca5581e31558d206fa4a7e0eb37bd
Teemu Harinen <teemuharinen1@gmail.com>
2023-12-06 15:29:57 +0200

    fixed 6.8

--- osa6/redux-anecdotes-main/src/App.jsx
+++ osa6/redux-anecdotes-main/src/App.jsx
@@ -1,49 +1,7 @@
-import { useSelector, useDispatch } from 'react-redux'
-import { increaseVote, createAnecdote } from './reducers/anecdoteReducer'
 
-const AnecdoteFrom = () => {
-  const dispatch = useDispatch()
+import AnecdoteFrom from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
 
-  const addAnecdote = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    console.log(content)
-    dispatch(createAnecdote(content))
-  }
-
-  return (
-    <>
-    <h2>create new</h2>
-      <form onSubmit={addAnecdote}>
-        <input name="anecdote"/>
-        <button type="submit">create</button>
-      </form>
-    </>
-  )
-}
-
-const AnecdoteList = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
-
-  return (
-    <>
-    <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => dispatch(increaseVote(anecdote.id))}>vote</button>
-          </div>
-        </div>
-      )}
-    </>
-  )
-}
 const App = () => {
   return (
     <div>
---
+++ osa6/redux-anecdotes-main/src/components/AnecdoteForm.jsx
@@ -0,0 +1,26 @@
+import { useDispatch } from 'react-redux'
+import { createAnecdote } from '../reducers/anecdoteReducer'
+
+const AnecdoteFrom = () => {
+  const dispatch = useDispatch()
+
+  const addAnecdote = (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    console.log(content)
+    dispatch(createAnecdote(content))
+  }
+
+  return (
+    <>
+    <h2>create new</h2>
+      <form onSubmit={addAnecdote}>
+        <input name="anecdote"/>
+        <button type="submit">create</button>
+      </form>
+    </>
+  )
+}
+
+export default AnecdoteFrom
