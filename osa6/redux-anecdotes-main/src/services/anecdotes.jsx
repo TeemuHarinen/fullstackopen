b7226b4423f38735872458247200a794a93a90ef
Teemu Harinen <teemuharinen1@gmail.com>
2023-12-16 08:16:49 +0200

    stream deck test

--- osa6/redux-anecdotes-main/src/services/anecdotes.jsx
+++ osa6/redux-anecdotes-main/src/services/anecdotes.jsx
@@ -11,8 +11,7 @@ const createNew = async (content) => {
   const response = await axios.post(baseUrl, obj)
   return response.data
 }
-// test
-// test 2
+
 const update = async (id, newObject) => {
   const response = await axios.put(`${baseUrl}/${id}`, newObject)
   return response.data
